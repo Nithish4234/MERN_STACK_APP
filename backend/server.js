bfc2a24a5f6bcccdf041f4b8489db9bd699df4f2
Nithish <potlapula.nithish@gmail.com>
2024-10-04 15:43:59 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ app.use('/api', apiRoutes);
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.state(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
 } else {
   app.get("/", (req, res) => {
